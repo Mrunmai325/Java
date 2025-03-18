41d10248b146735f3be2c7e94380e5eb2b0ace79
Mrunmai325 <2023bit049@sggs.ac.in>
2025-03-18 20:41:20 +0530

    suduku game-version 2

--- p10.java
+++ p10.java
@@ -1,24 +1,63 @@
+import java.util.Scanner;
 class Game{
-	static String[][] layout;
+	String[][] layout;
 	Game(int size){
 		layout=new String[size][size];
+		String num;
+		String[] p=new String[size];
 		for(int i=0; i<size; i++){
-			for(int j=0; j<size; j++){
-				layout[i][j]="";
+			p[i]=Integer.toString(i+1);
+		}
+		for(int i=0; i<size; i++){
+			for(int j=0; j<size; j++){  
+				num=p[(int)(System.nanoTime()%size)];
+			        while(!check(num, i, j)){
+					num=p[(int)(System.nanoTime()%size)];
+			        }
+			        layout[i][j]=num;
 			}
 		}
-		int digits=Integer.toString(layout.length).length();
-		layout=getElements(digits, layout);
-		layout=removeElements(digits, layout);
+		int length=Integer.toString(size).length();
+		for(int i=size*size/3; i>0; i--){
+			layout[(int)(System.nanoTime()%size)][(int)(System.nanoTime()%size)]=String.format("%" + length +"s", "");
+		}
 	}
 		
 	public static void main(String[] args){
 		int size=Integer.parseInt(args[0]);
 		Game s = new Game(size);
-		s.displayGrid(size, layout);
-	}	
+		s.displayGrid(size);
+		for(int i=0; i<size; i++){
+			for(int j=0; j<size; j++){
+				if(s.layout[i][j].equals(String.format("%"+Integer.toString(size).length()+"s", ""))){
+					System.out.println("enter element at " + (i+1) + " row "+ (j+1) +" column");
+					Scanner sc=new Scanner(System.in);
+					s.layout[i][j]=sc.nextLine();
+					s.displayGrid(size);
+				}
+			}
+		}		
+	}
+	
+	boolean check(String num, int row, int col){
+		int i=row, j=col;
+		while(j!=0){
+			j-=1;
+			if(layout[i][j]==num){
+				return false;
+			}
+		}
+		j=col;
+		while(i!=0){
+			i-=1;
+			if(layout[i][j]==num){
+				return false;
+			}
+		}
+		return true;
+	}
 			
-	void displayGrid(int size, String[][] layout){
+	void displayGrid(int size){
 		String horizontal="_".repeat(Integer.toString(size).length());
 		for(int ln=0; ln<=2*size; ln++){
 			for(int sy=2*size; sy>=0; sy--){
@@ -41,23 +80,5 @@ class Game{
 			}
 			System.out.print("\n");
 		}
-	}
-				
-	String[][] getElements(int digits, String[][] layout){
-		for(int i=0; i<layout.length; i++){
-			for(int j=0; j<layout.length; j++){
-				layout[i][j]=String.format("%" + digits +"s",(i+j)%layout.length+1);
-			}
-		}
-		return layout;
-	}
-
-
-	String[][] removeElements(int digits, String[][] layout){
-		int s=(layout.length)*(layout.length)/3;
-		for(int i=1; i<=s; i++){
-			layout[(int)(System.nanoTime()%layout.length)][(int)(System.nanoTime()%layout.length)]=String.format("%"+digits+"s", "");
-		}
-		return layout;
 	}
 }	
